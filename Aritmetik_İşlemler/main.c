915fdfe4582a2b7ce4fdc1f5cd61d6eee619bfc9
İshak Eren <ishakerenn1905@gmail.com>
2023-05-19 21:07:03 +0300

    s

---
+++ Aritmetik_İşlemler/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    //Direkt Aritmetik İşlemleri Yazdırabiliriz
+    printf("%d\n",21+3);
+    printf("%d\n",45*10);
+    printf("%d\n",450-12);
+    printf("%d\n",201%10);
+    printf("%d\n",45/10);
+    printf("%f\n",32.3*10.8);
+    
+    //Atanan değerin soluna -  + koyarak sayının işaret ile çarpılmasını sağlarız
+    int x=3;
+    int y=5;
+    
+    printf("%d\n %d\n",-x,-y);
+    
+    return 0;
+}
---
+++ Veri_Tipleri&Prntf/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int x= 10;
+    char y= 'I';
+    float z= 13.3;
+    double d= 13.333333;
+    
+    printf("%d %c %.1f %.3f\n",x,y,z,d);
+
+    return 0;
+}
